e47f58e3a645fbab20308c95ca3e9223ea1eaa92
DNAmaster10 <louishollobon@outlook.com>
2022-07-30 17:04:46 +0100

    qwufhwouiefhwiuf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use beryllium::{GlProfile, InitFlags, SDL, SdlGlAttr};
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = SDL::init(InitFlags::Everything).except("SDL is broke");
+    sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 3).unwrap();
+    sdl.gl_set_attribute(SdlGlAttr::MinorVersion, 3).unwrap();
+    sdl.gl_set_attribute(SdlGlAttr::Profile, GlProfile::Core).unwrap();
+    #[cfg(target_os = "macos")]
+    {
+        sdl
+            .gl_set_attribute(SdlGlAttr::Flags, ContextFlag::ForwardCompatible)
+            .unwrap();
+    }
 }
+
+
+// Language: rust
+// Path: DNAmaster10 | main.rs
